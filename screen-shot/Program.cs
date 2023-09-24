fab7a8217ac7cedf5670aa9ae910a387bec93562
mhuseynn <hmehdiyev2005@gmail.com>
2023-09-24 17:31:18 +0400

    s

--- screen-shot/Program.cs
+++ screen-shot/Program.cs
@@ -2,7 +2,7 @@
 using System.Drawing;
 using System.Drawing.Imaging;
 
-
+List<string> names = new List<string>(); 
 string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 string imagesFolderPath = Path.Combine(desktopPath, "Images");
 
@@ -13,12 +13,12 @@ if (!Directory.Exists(imagesFolderPath))
 
 const int screen_width = 1920; 
 const int screen_height = 1080;
-static string ss_and_save(string path)
+static string ss_and_save(string path,List<string> names)
 {
     DateTime now = DateTime.Now;
     string fileName = $"screenshot_{now:MM_dd_yyyy}.png";
     string filePath = Path.Combine(path, fileName);
-
+    names.Add(fileName);
     using (var bitmap = new Bitmap(screen_width ,screen_height))
     {
         using (var graphics = Graphics.FromImage(bitmap))
@@ -32,15 +32,27 @@ static string ss_and_save(string path)
     return filePath;
 }
 
-
+static void get_files(List<string> names)
+{
+    Console.WriteLine("Names: ");
+    foreach (var item in names)
+    {
+        Console.Write("File: ");
+        Console.WriteLine(item);
+    }
+}
 while (true)
 {
-    Console.WriteLine("Press enter for screenshot or any key stop...");
+    Console.WriteLine("Press enter for screenshot or [l] view files names or any key stop...");
     ConsoleKeyInfo key = Console.ReadKey();
     if (key.Key == ConsoleKey.Enter)
     {
-        ss_and_save(imagesFolderPath);
+        ss_and_save(imagesFolderPath,names);
         Console.WriteLine("Saved");
+    }
+    else if (key.Key == ConsoleKey.L)
+    {
+        get_files(names);
     }
     else
         break;
